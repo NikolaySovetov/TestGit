689a1ba62b4fa3c3a923789aebc2160e741d3135
Nikolay Sovetov <sovetov.k@gmail.com>
2022-06-18 18:37:41 +0300

    header

---
+++ header.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+
+class MyClass {
+  private:
+    int a;
+    int b;
+    int c;
+
+  public:
+    MyClass() {}
+    ~MyClass() {}
+
+    void DoSomething()  {}
+};
+
